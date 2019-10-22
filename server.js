a32735ecdc5fdda0fe97832342e55d3743059b42
N237097 <55752468+N237097@users.noreply.github.com>
2019-10-22 15:50:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on("disconnectiona", function(){
+  socket.on("disconnection", function(){
     console.log("Lappen disconnected");
   })
 });
