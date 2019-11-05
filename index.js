12c78af18eb1639d298b8fc3e91550a1661c4b3a
Luggalux <luca.reiteritaliano@isa-augsburg.com>
2019-11-05 15:41:10 +0100

    Added index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("js loaded");
+$(function () {
+    var socket = io();
+    $('form').submit(function(e){
+      e.preventDefault(); // prevents page reloading
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      return false;
+    });
+    $('#messages').append($('<li>').text('Message to add to chat'))
+  });
--- server.js
+++ server.js
@@ -14,11 +14,18 @@ app.get('/stylesheet.css', function(req, res){
   res.sendFile(__dirname + '/stylesheet.css');
 });
 
+app.get('/socket.io/socket.io.js', (req, res) => {
+  res.send(__dirname + '/socket.io/socket.io.js');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on("disconnect", function(){
     console.log("Lappen disconnected");
-  })
+  });
+  socket.on("chat message", function(msg){
+    console.log("User: " + msg);
+  });
 });
 
 http.listen(3000, function(){
