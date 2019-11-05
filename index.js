336a499abdbe32fe73c467cb9348fff565321080
N237097 <55752468+N237097@users.noreply.github.com>
2019-11-05 16:31:36 +0100

    Included users

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 $(function () {
     var socket = io();
+    var username = prompt("Please type in your username");
+    socket.emit(username);
+
     $('form').submit(function(e){
       e.preventDefault(); // prevents page reloading
       socket.emit('chat message', $('#m').val());
       $('#m').val('');
       return false;
     });
+
+    
     socket.on("chat message", function(msg){
       $('#messages').append($('<li>').text(msg));
     });
+
   });
