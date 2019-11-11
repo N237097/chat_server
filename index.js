80d15c4db12334d2065bf4bb8bd8615d2f7b5878
leoworm <leo.charles.p@gmail.com>
2019-11-11 08:56:34 +0100

    Added User List Functionality

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 $(function () {
     var socket = io();
     var username = prompt("Please type in your username");
-    socket.emit(username);
+    socket.emit('username', username);
 
     $('form').submit(function(e){
       e.preventDefault(); // prevents page reloading
       socket.emit('chat message', $('#m').val());
+      socket.emit('user_list')
       $('#m').val('');
       return false;
     });
 
-    
+    socket.on('user_list', (msg) => {
+      console.log(msg)
+      $('#names').empty()
+      for(var i=0; i < msg.length; i++){
+        console.log('in loop')
+        $('#names').append($('<li>').text(msg[i]))
+      }
+    })
     socket.on("chat message", function(msg){
       $('#messages').append($('<li>').text(msg));
     });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var util = require('util')
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -21,17 +22,27 @@ app.get('/socket.io/socket.io.js', (req, res) => {
 var username = {}
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on("disconnect", function(){
-    console.log("Lappen disconnected");
+  socket.once("disconnect", function(){
+    console.log(username[socket.id] + " disconnected");
   });
+
+  //TODO Switch from socket.id to LocalStorage or Cookies 
   socket.on("username", function(user){
+    console.log(user + ' has connected');
     username[socket.id] = user;
   })
   socket.on("chat message", function(msg){
     console.log(username[socket.id] + ": " + msg);
     io.emit("chat message", username[socket.id] + ': ' + msg);
   });
+  socket.on('user_list', (msg) => {
+    //console.log(io.sockets.connected)
+    var users = []
+    for(key in io.sockets.connected){
+      users.push(username[key])
+    }
+    socket.emit('user_list', users)//util.inspect(io.sockets.connected))
+  })
 });
 
 http.listen(3000, function(){
