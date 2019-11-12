13a24b817fb38b8110296edd1f691462996c4b0b
leoworm <leo.charles.p@gmail.com>
2019-11-12 13:20:41 +0100

    Added sessions based on cookies, made scrollbar sticky, added basic anti spam measures

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 $(function () {
     var socket = io();
-    var username = prompt("Please type in your username");
-    socket.emit('username', username);
 
+    //TODO: Check if message is empty
     $('form').submit(function(e){
-      e.preventDefault(); // prevents page reloading
-      socket.emit('chat message', $('#m').val());
-      socket.emit('user_list')
-      $('#m').val('');
+      e.preventDefault(); // prevents page reloading when form is submitted
+      if($('#m').val()){
+        socket.emit('chat message', $('#m').val());
+        socket.emit('user_list')
+        $('#m').val('');
+      }
       return false;
     });
 
     socket.on('user_list', (msg) => {
-      console.log(msg)
-      $('#names').empty()
+      $('.names').empty()
       for(var i=0; i < msg.length; i++){
-        console.log('in loop')
-        $('#names').append($('<li>').text(msg[i]))
+        $('.names').append($('<li>').text(msg[i]))
       }
     })
     socket.on("chat message", function(msg){
-      $('#messages').append($('<li>').text(msg));
+      var tempScroll = $(window).scrollTop()
+      if(tempScroll+1 >= $(document).height()-$(window).height()){
+        console.log('should stick')
+        $('#messages').append($('<li>').text(msg));
+        $(window).scrollTop($(document).height()-$(window).height())
+      }else{
+        console.log('should not stick')
+        $('#messages').append($('<li>').text(msg));
+        $(window).scrollTop(tempScroll)
+      }
+      
+      console.log($(window).scrollTop())
+      console.log($(document).height()-$(window).height())
     });
-
+    
   });
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var util = require('util')
+var cookieParser = require('cookie-parser') 
+var cookie = require('cookie')
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  if(req.cookies.username){
+    res.sendFile(__dirname + '/index.html');
+  }else{
+    res.sendFile(__dirname + '/login.html')
+  }
 });
+
+//TODO: Make Cookies temporary
+app.post('/register', (req, res) => {
+  console.log(req.body)
+  res.cookie('username', req.body.username)
+  res.sendFile(__dirname + '/index.html')
+})
 
 app.get('/index.js', function(req, res){
   res.sendFile(__dirname + '/index.js');
@@ -16,35 +34,39 @@ app.get('/stylesheet.css', function(req, res){
 });
 
 app.get('/socket.io/socket.io.js', (req, res) => {
-  res.send(__dirname + '/socket.io/socket.io.js');
+  res.sendFile(__dirname + '/socket.io/socket.io.js');
 });
 
 var username = {}
 
 io.on('connection', function(socket){
+  username[socket.id] = cookie.parse(socket.handshake.headers.cookie).username
+  console.log(username[socket.id] + ' has connected');
+  updateUsers()
+
   socket.once("disconnect", function(){
     console.log(username[socket.id] + " disconnected");
+    updateUsers()
   });
 
   //TODO Switch from socket.id to LocalStorage or Cookies 
-  socket.on("username", function(user){
-    console.log(user + ' has connected');
-    username[socket.id] = user;
-  })
+
   socket.on("chat message", function(msg){
     console.log(username[socket.id] + ": " + msg);
     io.emit("chat message", username[socket.id] + ': ' + msg);
   });
-  socket.on('user_list', (msg) => {
-    //console.log(io.sockets.connected)
-    var users = []
-    for(key in io.sockets.connected){
-      users.push(username[key])
-    }
-    socket.emit('user_list', users)//util.inspect(io.sockets.connected))
-  })
 });
 
+function updateUsers(){
+  var users = []
+  for(key in io.sockets.connected){
+    if(username[key] != null){
+      users.push(username[key])
+    }
+  }
+  io.emit('user_list', users)
+}
+
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('Listening on *:3000');
 });
